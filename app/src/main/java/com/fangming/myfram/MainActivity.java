ae6b6a8bf406076bce921166f49c005f379dd928
fangming <2385313404@qq.com>
2016-08-09 16:40:18 +0800

    新增MyUtill，labray

--- app/src/main/java/com/fangming/myfram/MainActivity.java
+++ app/src/main/java/com/fangming/myfram/MainActivity.java
@@ -1,13 +1,21 @@
 package com.fangming.myfram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String str="{\"id\":1,\"pwd\":321}";
+        Gson gson=new Gson();
+        Person p= gson.fromJson(str,Person.class);
+        TextView btn_clic = (TextView) findViewById(R.id.btn_clic);
+        btn_clic.setText(p.toString());
     }
 }
---
+++ app/src/main/java/com/fangming/myfram/Person.java
@@ -0,0 +1,15 @@
+package com.fangming.myfram;
+
+/**
+ * Created by fangming on 2016-7-28.
+ */
+public class Person {
+    private int id;
+
+    private int age;
+
+    @Override
+    public String toString() {
+        return id+":"+age;
+    }
+}
