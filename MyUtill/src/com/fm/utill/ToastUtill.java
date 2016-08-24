51e12148c5242382edf6104ccd87a98fb0a06b5d
fangming <2385313404@qq.com>
2016-08-24 17:23:25 +0800

    修改配置文件

--- MyUtill/src/com/fm/utill/ToastUtill.java
+++ MyUtill/src/com/fm/utill/ToastUtill.java
@@ -1,7 +1,22 @@
 package com.fm.utill;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
+ * Toast公用类
  * Created by fangming on 2016-8-24.
  */
 public class ToastUtill {
+    private static Toast toast;
+
+    public static void showToast(Context context,String msg){
+        if(toast==null){
+            toast=Toast.makeText(context,msg,Toast.LENGTH_SHORT);
+        }else{
+            toast.setText(msg);
+        }
+        toast.show();
+    }
+
 }
