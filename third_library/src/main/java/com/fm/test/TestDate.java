96b90e323917900363cdef37fb3d86dc9db889b2
fangming <2385313404@qq.com>
2016-12-22 14:09:21 +0800

    提交第三方包

---
+++ third_library/src/main/java/com/fm/test/TestDate.java
@@ -0,0 +1,15 @@
+package com.fm.test;
+
+import java.util.Date;
+
+/**
+ * Created by fangming on 2016-11-24.
+ */
+public class TestDate {
+
+    public static void main(String[] args){
+        System.out.println(new Date());
+    }
+
+
+}
